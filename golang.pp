ae21f2f45c52b434370ba978e805e8cb623ff715
stephen-marshall-moore <stephen.moore@linux.com>
2013-02-20 01:17:26 +0000

    Committer: stephen-marshall-moore <stephen.mooret@linux.com> 	modified:   golang.pp 	new file:   templates/etc/profile.d/golang.sh 	modified:   wiki.pp   put PATH and GOPATH into seperate file to be added

--- golang.pp
+++ golang.pp
@@ -11,4 +11,14 @@ exec { 'buildgo':
     command => '/usr/local/go/src/all.bash',
     cwd => '/usr/local/go/src',
     creates => '/usr/local/go/bin/go'
-}    
+}
+
+file { 'golang.sh':
+    require => Exec['buildgo'],
+    ensure => present,
+    path => '/etc/profile.d/golang.sh',
+    source => '/root/wiki-puppet-config/templates/etc/profile.d/golang.sh',
+    owner => root,
+    group => root,
+    mode => 0644
+}   
